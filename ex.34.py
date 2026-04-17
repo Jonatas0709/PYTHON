1907998dd89cd2e769ed639e44e1a66ee9b10b64
Jhonny leender <alves.jhonny@escola.pr.gov.br>
2026-04-17 16:31:13 -0300

    Add files via upload

--- ex.34.py
+++ ex.34.py
@@ -1,14 +1,14 @@
-# Exercício 34
+#Exercicio. 34
 #Contar quantos números foram digitados
 
 contador = 0
 
 while True:
-    numero = int(input("Digite um número (0 para sair): "))
+    numero= int(input("Digite um número (8 para sair): "))
 
-if numero == 0:
-    break
+    if numero == 0:
+        break
 
-contador = contador + 1
+        contador contador + 1
 
-print("Quantidade:", contador)
+        print("Quantidade:", contador)
---
+++ ex.40.py
@@ -0,0 +1,10 @@
+# Exercício 40
+# Função com parâmetro
+
+def saudacao (nome):
+    print("Olá", nome)
+
+    input = ("Nome: ")
+
+
+    saudacao (nome)
